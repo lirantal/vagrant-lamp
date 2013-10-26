4bef8ff0305911be2a319d0c0d14872e59b16af3
Liran Tal <liran.tal@gmail.com>
2013-10-26 18:26:33 +0300

    fixed class instantiation priority to make sure some modules run before others

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,7 +1,10 @@
 import 'classes/*'
 
-include base
+# define ordering
+Class['base']->Class['apache']->Class['mysql']->Class['php']
 
+# instantiate class resources
+include base
 include apache
 include mysql
 include php
